cdf5515b24664b0cdfde98b0cba6eba24bf15721
Rahulsingh912 <110731456+Rahulsingh912@users.noreply.github.com>
2022-08-07 10:33:05 +0530

    Add files via upload

---
+++ max min.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+    int i;
+    int min=INT_MAX;
+    int max=INT_MIN;
+    for(int i=0;i<n;i++){
+        if(a[i]<min){
+            min=a[i];
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(a[i]>max){
+            max=a[i];
+        }
+    }
+   // getMinMax.first=min;
+  //  getMinMax.second=max;
+    return make_pair(min,max);
+}
